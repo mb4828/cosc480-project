09233a8b5cdd2b7e717dcc612d574cc229d6560d
Matt Brauner <mbrauner@colgate.edu>
2015-04-02 12:30:01 -0400

    new course template works now. needs to be wired

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,10 +7,10 @@ class CoursesController < ApplicationController
     def create
       if @course = Course.create(create_update_params)
         flash[:notice] = "New course #{@Course.name} successfully created"
-        redirect_to schedule_courses_path
+        redirect_to edit_schedule_path
       else
         flash[:warning] = "Course couldn't be created"
-        redirect_to schedule_courses_path
+        redirect_to edit_schedule_course_path
       end
     end
 
@@ -22,10 +22,10 @@ class CoursesController < ApplicationController
         @course = Course.find(params[:id])
         if @course.update(create_update_params)
           flash[:notice] = "Update was successful"
-          redirect_to schedule_courses_path
+          redirect_to edit_schedule_path
         else
           flash[:warning] = "There was an error, the course wasn't updated"
-          redirect_to schedule_courses_path
+          redirect_to edit_schedule_course_path
         end
     end
 
@@ -34,10 +34,10 @@ class CoursesController < ApplicationController
         @course.destroy
         if @course.destroy(create_update_params)
           flash[:notice] = "Successfully removed"
-          redirect_to schedule_courses_path
+          redirect_to edit_schedule_path
         else
           flash[:warning] = "There was an error, the course wasn't removed"
-          redirect_to schedule_courses_path
+          redirect_to edit_schedule_path
         end
     end
 
