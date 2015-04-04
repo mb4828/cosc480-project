3eecd68a83013860cee4e052cd48bc8f3845f20b
lsslade <lslade@colgate.edu>
2015-04-04 12:33:45 -0400

    worked on test coverage

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-Rspec.describe CoursesController, type: :controler do
+RSpec.describe CoursesController, type: :controller do
 
-  describle "root route" do 
+  describe "root route" do 
     it "routes to schedules#new" do 
-      expect (:get => '/' ).to route_to(:controller => "schedules",:action => "new"
+      expect(:get => '/').to route_to(:controller => "schedules",:action => "new")
     end
   end
 
- describe "GET #index" do
+ describe "GET #new" do
     it "routes correctly" do
-      get :index
+      get :index 
       expect(response.status).to eq(200)
     end
   end
@@ -36,18 +36,18 @@ Rspec.describe CoursesController, type: :controler do
 
     it "should show success flash message and redirect to index upon successful creation" do
       c = Course.new
-      expect(Course).to receive (:new) {c}
-      expect(c).to receive (:save) {true}
-      post :create, {:course => ("name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am"}
+      expect(Course).to receive (:new){c}
+      expect(c).to receive(:save){true}
+      post :create,{:course => {"name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am", :schedule_id => "1"}, :schedule_id => "1"}
       expect(response).to redirect_to (schedule_courses_path)
       flash['notice'] != nil
     end
 
     it "should show failure flash message and redirect to index upon failed creation" do
       c = Course.new
-      expect(Course).to receive (:new) {c}
-      expect(c).to receive (:save) {nil}
-      post :create, {:course => ("name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am"}
+      expect(Course).to receive(:new){c}
+      expect(c).to receive(:save){nil}
+      post :create,{:course => {"name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am", :schedule_id => "1"}, :schedule_id => "1"}
       expect(response).to redirect_to (schedule_courses_path)
       flash['warning'] != nil
     end
@@ -58,18 +58,18 @@ Rspec.describe CoursesController, type: :controler do
 
     it "should show success flash message and redirect to index upon successful update" do
       c = Course.new
-      expect(Course).to receive (:find).with("1") {c}
-      expect(c).to receive (:update) {true}
-      put :update, {:course => ("id" => "1", "name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am"}
+      expect(Course).to receive(:find).with("1"){c}
+      expect(c).to receive(:update){true}
+      put :update,{:course => {"id" => "1", "name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am", :schedule_id => "1"}, :schedule_id => "1"}
       expect(response).to redirect_to (schedule_courses_path)
       flash['notice'] != nil
     end
 
     it "should show failure flash message and redirect to index upon failed creation" do
       c = Course.new
-      expect(Course).to receive (:find).with("1") {c}
-      expect(c).to receive (:update) {nil}
-      put :update, {:course => ("id" => "1", "name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am"}
+      expect(Course).to receive(:find).with("1"){c}
+      expect(c).to receive(:update){nil}
+      put :update,{:course => {"id" => "1", "name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am", :schedule_id => "1"}, :schedule_id => "1"}
       expect(response).to redirect_to (schedule_courses_path)
       flash['warning'] != nil
     end
@@ -79,18 +79,18 @@ Rspec.describe CoursesController, type: :controler do
 
     it "should show success flash message and redirect to index upon successful deletion" do
       c = Course.new
-      expect(Course).to receive (:find).with("1") {c}
-      expect(c).to receive (:delete) {true}
-      delete :destroy, {:course => ("id"=>"1","name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am"}
+      expect(Course).to receive(:find).with("1"){c}
+      expect(c).to receive(:delete){true}
+      delete :destroy,{:course => {"id"=>"1","name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am", :schedule_id => "1"}, :schedule_id => "1"}
       expect(response).to redirect_to (schedule_courses_path)
       flash['notice'] != nil
     end
 
     it "should show failure flash message and redirect to index upon failed deletion" do
       c = Course.new
-      expect(Course).to receive (:find).with("1") {c}
-      expect(c). to receive (:delete) {nil}
-      delete :destroy, {:course => ("id"=>"1","name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am"}
+      expect(Course).to receive(:find).with("1"){c}
+      expect(c). to receive(:delete){nil}
+      delete :destroy,{:course => {"id"=>"1","name" => "intro", "description" => "cosc 101", "sunday" => false, "monday" => true, "tuesday" => false, "wednesday" => true, "thursday" => false, "friday" => true, "saturday" => false, "start_time" => "10:20am", "end_time" => "11:10am", :schedule_id => "1"}, :schedule_id => "1"}
       expect(response).to redirect_to (schedule_courses_path)
       flash['warning'] != nil
     end
