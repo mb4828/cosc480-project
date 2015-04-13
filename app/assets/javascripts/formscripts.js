9e4e0eb484c63be0af487403d1d8c597b0b9d2b6
Matt Brauner <mbrauner@colgate.edu>
2015-04-13 18:37:58 -0400

    cosmetic updates

--- app/assets/javascripts/formscripts.js
+++ app/assets/javascripts/formscripts.js
@@ -1,29 +1,34 @@
-/* 
-toggles the actual checkboxes on and off for the days of the week buttons
-(the checkboxes that are displayed on the form are "fake" bootstrap buttons and have "real" checkbox counterparts)
-*/
-function checkToggle(id, day){
-    courseform.elements[day + "-check"].checked = !courseform.elements[day + "-check"].checked;
-}
+var formscripts = (function(){
+  
+  return {
 
-/* 
-sets the bootstrap form buttons to match the actual form checkboxes
-*/
-function setButtons(id){
-    courseform.elements["sun-check"].checked ? $('#sun').addClass('active').attr('aria-pressed','true') : $('#sun').removeClass('active').attr('aria-pressed','false');
+    /* toggles the actual checkboxes on and off for the days of the week buttons
+     * (the checkboxes that are displayed on the form are "fake" bootstrap buttons and 
+     * have "real" checkbox counterparts) */
+    checkToggle: function(id, day){
+      courseform.elements[day + "-check"].checked = !courseform.elements[day + "-check"].checked;
+    },
+
+    /* sets the bootstrap form buttons to match the actual form checkboxes */
+    setButtons: function(id){
+      courseform.elements["sun-check"].checked ? $('#sun').addClass('active').attr('aria-pressed','true') : $('#sun').removeClass('active').attr('aria-pressed','false');
     courseform.elements["mon-check"].checked ? $('#mon').addClass('active').attr('aria-pressed','true') : $('#mon').removeClass('active').attr('aria-pressed','false');
     courseform.elements["tue-check"].checked ? $('#tue').addClass('active').attr('aria-pressed','true') : $('#tue').removeClass('active').attr('aria-pressed','false');
     courseform.elements["wed-check"].checked ? $('#wed').addClass('active').attr('aria-pressed','true') : $('#wed').removeClass('active').attr('aria-pressed','false');
     courseform.elements["thu-check"].checked ? $('#thu').addClass('active').attr('aria-pressed','true') : $('#thu').removeClass('active').attr('aria-pressed','false');
     courseform.elements["fri-check"].checked ? $('#fri').addClass('active').attr('aria-pressed','true') : $('#fri').removeClass('active').attr('aria-pressed','false');
     courseform.elements["sat-check"].checked ? $('#sat').addClass('active').attr('aria-pressed','true') : $('#sat').removeClass('active').attr('aria-pressed','false');
-}
 
-/*
-for graceful degredation, javascript powered buttons will only be displayed if user has javascript 
-enabled in their browser
-*/
-function setupForm(id){
-  $('.checkboxes').hide();
-  $('.bootstrap-button').show();
-}
+    },
+
+    /* for graceful degredation, javascript powered buttons will only be displayed if user has javascript 
+     * enabled in their browser */
+    setupForm: function(id){
+      $('.checkboxes').hide();
+      $('.bootstrap-button').show();
+    },
+
+  }
+
+}());
+
--- app/assets/javascripts/schedulescripts.js
+++ app/assets/javascripts/schedulescripts.js
@@ -1,128 +1,133 @@
-var INITIAL_OFFSET = 45;        // initial distance from top of "day" div to beginning of 6am hour in pixels
-var HOUR_HEIGHT = 35;         // height of each hour in pixels
+var schedulescripts = (function() {
 
-/* HELPER: takes a string containing a time (e.g. 9:02am) and returns the number of minutes since 6am,
- * assuming time is after 6am and before 11pm, otherwise -1 is returned */
-function getMinutes(timestr){
-    var hour = parseInt(timestr.slice(0, timestr.indexOf(":")));
-    var min = parseInt(timestr.slice(-4,-2));
-    var pmflag = timestr.search("pm");
+  var INITIAL_OFFSET = 45;      // initial distance from top of "day" div to beginning of 6am hour in pixels
+  var HOUR_HEIGHT = 35;         // height of each hour in pixels
 
-    if (pmflag == -1 && (hour < 6 || hour == 12)) return -1;        // return -1 if time is before 6am
-    if (pmflag != -1 && hour == 11 && min > 0) return -1;           // return -1 if time is after 11pm
+  return {
 
-    var minutes = min;
+    /* HELPER: takes a string containing a time (e.g. 9:02am) and returns the number of minutes since 6am,
+     * assuming time is after 6am and before 11pm, otherwise -1 is returned */
+    getMinutes: function(timestr){
+      var hour = parseInt(timestr.slice(0, timestr.indexOf(":")));
+      var min = parseInt(timestr.slice(-4,-2));
+      var pmflag = timestr.search("pm");
 
-    if (pmflag == -1){
-        minutes += (hour - 6) * 60;         // morning
-    } else {
-        if (hour == 12){
-            minutes += 360;                 // 12pm
-        } else {
-            minutes += 360 + hour * 60;     // afternoon/evening
+      if (pmflag == -1 && (hour < 6 || hour == 12)) return -1;        // return -1 if time is before 6am
+      if (pmflag != -1 && hour == 11 && min > 0) return -1;           // return -1 if time is after 11pm
+
+      var minutes = min;
+
+      if (pmflag == -1){
+          minutes += (hour - 6) * 60;         // morning
+      } else {
+          if (hour == 12){
+              minutes += 360;                 // 12pm
+          } else {
+              minutes += 360 + hour * 60;     // afternoon/evening
+          }
+      }
+
+      return minutes;
+    },
+
+    /* HELPER: takes the start time in minutes since 6am and returns the offset in pixels that the classbox should have */
+    getOffset: function(startmin){
+      var hours = startmin / 60;
+      var rest = startmin % 60;
+      return Math.floor(INITIAL_OFFSET + (hours * HOUR_HEIGHT) + (rest / HOUR_HEIGHT));
+    },
+
+    /* HELPER: takes the start/end time in minutes since 6am and returns the height in pixels that the classbox should have,
+     * assuming end is after start, otherwise -1 is returned */
+    getHeight: function(startmin, endmin) {
+      var diff = endmin - startmin;
+
+      if (diff < 0) return -1;        // return -1 if start is after end
+
+      var height = Math.ceil(diff * (HOUR_HEIGHT / 60));
+
+      if (height < 20) {
+          return 20;                  // make sure the box is big enough for the text!
+      } else {
+          return height;
+      }
+    },
+
+    /* takes a day (e.g. "sun"), descriptive text, a start time string (e.g. "10:15am"), and an end time string and
+     * creates a box in the appropriate day of the week column on the page */
+    makeBox: function(id, day, desc, start, end){
+      var startmin = this.getMinutes(start);
+      var endmin = this.getMinutes(end);
+
+      if (startmin == -1 || endmin == -1) return -1;     // something is invalid so give up
+
+      var offset = this.getOffset(startmin);
+      var height = this.getHeight(startmin, endmin);
+
+      if (height == -1) return -1;                       // something is invalid so give up
+
+      // construct a new classbox and add to the DOM
+      var classbox = "<div class='cbox" + id + " classbox' style='" +
+                          "height:" + height + "px; " +
+                          "top:" + offset + "px;" +
+                          "line-height:" + height + "px;" +
+                          //"background: linear-gradient(" + colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ");" +
+                      "'>" + desc + "</td></div>";
+      $('#'+day).append(classbox);
+    },
+
+    /* removes classboxes for a particular course from the visualized schedule */
+    deleteSchedule: function(id){ $('.cbox'+id).remove(); },
+
+    /* generates classboxes for a particular course on the visualized schedule */
+    createSchedule: function(id){
+        // delete all previously created classboxes for this course
+        this.deleteSchedule(id);
+
+        // create new boxes
+        var desc = $("#course-"+id+"-desc").text();
+        var start = $("#course-"+id+"-start").text();
+        var end = $("#course-"+id+"-end").text();
+        //var color = $('#course-'+id+'-color').val();
+
+        if ($("#course-"+id+"-sun").text() == "true") this.makeBox(id, 'sun', desc, start, end);
+        if ($("#course-"+id+"-mon").text() == "true") this.makeBox(id, 'mon', desc, start, end);
+        if ($("#course-"+id+"-tue").text() == "true") this.makeBox(id, 'tue', desc, start, end);
+        if ($("#course-"+id+"-wed").text() == "true") this.makeBox(id, 'wed', desc, start, end);
+        if ($("#course-"+id+"-thu").text() == "true") this.makeBox(id, 'thu', desc, start, end);
+        if ($("#course-"+id+"-fri").text() == "true") this.makeBox(id, 'fri', desc, start, end);
+        if ($("#course-"+id+"-sat").text() == "true") this.makeBox(id, 'sat', desc, start, end);
+
+        // update form disp colors
+        //$('#disp'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ')');
+
+        // install mouseover handlers to do fancy highlighting magic
+        /*    
+        function enter(){
+            $('.cbox'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.4) + "," + color + ')');
+            $('.cbox'+id).css('border', '1px solid #DAA520');
+            $('#disp'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.4) + "," + color + ')');
+            $('#disp'+id).css('border', '1px solid #DAA520');
         }
-    }
 
-    return minutes;
-}
+        function leave(){
+            $('.cbox'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ')');
+            $('.cbox'+id).css('border', '1px solid gray');
+            $('#disp'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ')');
+            $('#disp'+id).css('border', '1px solid gray');
+        }
+        
+        $('.cbox'+id).mouseenter(function(){enter()});
+        $('#disp'+id).mouseenter(function(){enter()});
+        $('.cbox'+id).mouseleave(function(){leave()});
+        $('#disp'+id).mouseleave(function(){leave()});
+        */
+        // disable selection
+        $('.cbox'+id).addClass('noselect');
 
-/* HELPER: takes the start time in minutes since 6am and returns the offset in pixels that the classbox should have */
-function getOffset(startmin){
-    var hours = startmin / 60;
-    var rest = startmin % 60;
-    return Math.floor(INITIAL_OFFSET + (hours * HOUR_HEIGHT) + (rest / HOUR_HEIGHT));
-}
+        // fade the boxes in
+        $('.cbox'+id).fadeIn(600);
+    },
+  }
 
-/* HELPER: takes the start/end time in minutes since 6am and returns the height in pixels that the classbox should have,
- * assuming end is after start, otherwise -1 is returned */
-function getHeight(startmin, endmin) {
-    var diff = endmin - startmin;
-
-    if (diff < 0) return -1;        // return -1 if start is after end
-
-    var height = Math.ceil(diff * (HOUR_HEIGHT / 60));
-
-    if (height < 20) {
-        return 20;                  // make sure the box is big enough for the text!
-    } else {
-        return height;
-    }
-}
-
-/* takes a day (e.g. "sun"), descriptive text, a start time string (e.g. "10:15am"), and an end time string and
- * creates a box in the appropriate day of the week column on the page */
-function makeBox(id, day, desc, start, end){
-    var startmin = getMinutes(start);
-    var endmin = getMinutes(end);
-
-    if (startmin == -1 || endmin == -1) return -1;     // something is invalid so give up
-
-    var offset = getOffset(startmin);
-    var height = getHeight(startmin, endmin);
-
-    if (height == -1) return -1;                       // something is invalid so give up
-
-    // construct a new classbox and add to the DOM
-    var classbox = "<div class='cbox" + id + " classbox' style='" +
-                        "height:" + height + "px; " +
-                        "top:" + offset + "px;" +
-                        "line-height:" + height + "px;" +
-                        //"background: linear-gradient(" + colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ");" +
-                    "'>" + desc + "</td></div>";
-    $('#'+day).append(classbox);
-}
-
-/* removes classboxes for a particular course from the visualized schedule */
-function deleteSchedule(id){
-    $('.cbox'+id).remove();
-}
-
-/* generates classboxes for a particular course on the visualized schedule */
-function createSchedule(id){
-    // delete all previously created classboxes for this course
-    deleteSchedule(id);
-
-    // create new boxes
-    var desc = $("#course-"+id+"-desc").text();
-    var start = $("#course-"+id+"-start").text();
-    var end = $("#course-"+id+"-end").text();
-    //var color = $('#course-'+id+'-color').val();
-
-    if ($("#course-"+id+"-sun").text() == "true") makeBox(id, 'sun', desc, start, end);
-    if ($("#course-"+id+"-mon").text() == "true") makeBox(id, 'mon', desc, start, end);
-    if ($("#course-"+id+"-tue").text() == "true") makeBox(id, 'tue', desc, start, end);
-    if ($("#course-"+id+"-wed").text() == "true") makeBox(id, 'wed', desc, start, end);
-    if ($("#course-"+id+"-thu").text() == "true") makeBox(id, 'thu', desc, start, end);
-    if ($("#course-"+id+"-fri").text() == "true") makeBox(id, 'fri', desc, start, end);
-    if ($("#course-"+id+"-sat").text() == "true") makeBox(id, 'sat', desc, start, end);
-
-    // update form disp colors
-    //$('#disp'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ')');
-
-    // install mouseover handlers to do fancy highlighting magic
-    /*    
-    function enter(){
-        $('.cbox'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.4) + "," + color + ')');
-        $('.cbox'+id).css('border', '1px solid #DAA520');
-        $('#disp'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.4) + "," + color + ')');
-        $('#disp'+id).css('border', '1px solid #DAA520');
-    }
-
-    function leave(){
-        $('.cbox'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ')');
-        $('.cbox'+id).css('border', '1px solid gray');
-        $('#disp'+id).css('background', 'linear-gradient('+ colorLuminance(color, 0.2) + "," + colorLuminance(color, -0.2) + ')');
-        $('#disp'+id).css('border', '1px solid gray');
-    }
-    
-    $('.cbox'+id).mouseenter(function(){enter()});
-    $('#disp'+id).mouseenter(function(){enter()});
-    $('.cbox'+id).mouseleave(function(){leave()});
-    $('#disp'+id).mouseleave(function(){leave()});
-    */
-    // disable selection
-    $('.cbox'+id).addClass('noselect');
-
-    // fade the boxes in
-    $('.cbox'+id).fadeIn(600);
-}
+}());
