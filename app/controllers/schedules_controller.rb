3d8c314cac3432e5c45e4d8de2bf8f9a2f5d0e78
Zack Smith <zjsmith@colgate.edu>
2015-04-15 13:31:07 -0400

    Added lock functionality and zombie cleanup

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -13,24 +13,28 @@ class SchedulesController < ApplicationController
 
     def edit
         @schedule = Schedule.find(params[:id])
+        if @schedule.locked == true 
+            flash[:warning] = "This schedule cannot be altered"
+            redirect_to schedule_path
+        end
         @courses = @schedule.courses
     end
 
     def update
         # create the user object corresponding to the params 
         @user = User.create(:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email])
-
         # make sure schedule and user object are linked (done via has_many & belongs_to
         @schedule = Schedule.find(params[:id])
         @schedule.user_id = @user.id
 
-        # somehow lock/disable editing - make sure that you can't access the edit view anymore
-        # todo: implement this feature so it actually works!!
+        #somehow lock/disable editing
         @schedule.locked = true
         @schedule.save
 
         # redirect to a "permalink" which for now can just be the default route for the course
         redirect_to schedule_path
+        # todo potentially add zombie cleanup here?
+        
     end
 
     def show
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,17 @@
 class Schedule < ActiveRecord::Base
   has_many :courses
   belongs_to :user
+
+  def zombie ()
+    Schedule.all.each do |schedule|
+        if schedule.locked == false
+            update = schedule.updated_at.to_i
+            currtime = Time.now.to_i
+            if (currtime-update) > 3600 #3600 seconds in an hour
+                schedule.destroy
+            end
+        end
+    end
+  end
+    
 end
