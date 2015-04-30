751208d0138a3176ff0bbff9b5cfac6114792609
Matt Brauner <mbrauner@colgate.edu>
2015-04-30 19:43:36 -0400

    index page

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -6,13 +6,8 @@ class SchedulesController < ApplicationController
     def index
     end
 
-    # This is the root route as the app is currently defined.
-    # My thinking is that you can immediately initialize a schedule object here
-    # (even though it will be empty) and allow the user to start adding things.
-    # Future links back (such as from the new courses page) will go back to
-    # the update route.
     def new
-	@schedule = Schedule.create
+	      @schedule = Schedule.create
         render "new"
         cleanup_zombies
     end
